8cf513f51c574b88de54d4d14c9d2c6c63f16a06
nkevy <yvekhaon@gmail.com>
2020-05-24 10:45:03 -0700

    the libnk

---
+++ Mtable.py
@@ -0,0 +1,56 @@
+import os
+
+fer="[MTABLE FATAL ERROR] "
+class Mtable:
+	# constructor 
+	def __init__(self,path_to=""):
+		if not os.path.exists(path_to):
+			print(fer+"constructor input path not valid")
+			raise TypeError
+		self.row = []
+		self.col = []
+		self.rcomp = []
+		self.ccomp = []
+		self.in_file = open(path_to,"r")
+		self.out_file = None
+		# complete the row and col lists
+		if ".csv" in path_to:
+			for line in self.in_file:
+				add = self.parse_csv_line(line.strip(" "))
+				self.row.append(add)
+		if ".tsv" in path_to:
+			for line in self.in_file:
+				add = self.parse_csv_line(line.strip(" "))
+				self.row.append(add)
+		for i in range(len(self.row)):
+			for j in range(len(self.row[i])):
+				try:
+					item = self.row[i][j]
+					
+	# parse a line of csv
+	def parse_csv_line(self,line=""):
+		return line.split(',')
+	# parse a line of tsv
+	def parse_tsv_line(self,line=""):
+		return line.split('\t')
+	# getter for row
+	def get_row(self,index=0):
+		if index>=len(self.row):
+			return None
+		return self.row[index]
+	# getter for col
+	def get_col(self,index=0):
+		if index>=len(self.col):
+			return None
+		return self.col[index]
+	# getter for rcomp
+	def get_ccomp(self,index=0):
+		if index>=len(self.ccomp):
+			return None
+		return self.ccomp[index]
+	# getter for ccomp
+	def get_rcomp(self,index=0):
+		if index>=len(self.rcomp):
+			return None
+		return self.rcomp[index]
+# EOF
